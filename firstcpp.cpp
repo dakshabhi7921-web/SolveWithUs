d718ac223feddf4fa425fd314161543f3c10328a
dakshabhi7921-web <dakshabhi7921@gmail.com>
2026-03-17 18:02:35 +0530

    Enhance complex class with user input and comments
    Updated complex class to include user input for complex numbers and added comments for clarity.

--- firstcpp.cpp
+++ firstcpp.cpp
@@ -2,52 +2,65 @@
 using namespace std;
 
 class complex{
+    //initial
     private:
         float real;
         float imag;
-
+    
     public:
-        complex(){
+        complex(){                //constructor
             real = 0.0f;
             imag = 0.0f;
         }
-
-        void setData(float r, float i){
+            
+        void setData(float r, float i){        // set data
             real = r;
             imag = i;
         }
 
-        void add(complex c1, complex c2){
+        void add(complex c1, complex c2){                //Addition Function
             real = c1.real + c2.real;
             imag = c1.imag + c2.imag;
         }
 
-        void sub(complex c1, complex c2){
+        void sub(complex c1, complex c2){                //Subtraction Function
             real = c1.real - c2.real;
             imag = c1.imag - c2.imag;
         }
 
-        void multiply(complex c1, complex c2){
+        void multiply(complex c1, complex c2){            //Multiplication function
             real = c1.real * c2.real - c1.imag * c2.imag;
             imag = c1.real * c2.imag + c1.imag * c2.real;
         }
 
-        void divide(complex c1, complex c2){
-            int denominator = c2.real * c2.real + c2.imag * c2.imag;
+        void divide(complex c1, complex c2){                        //Division Function
+            int denominator = c2.real * c2.real + c2.imag * c2.imag;        
             real = float(c1.real * c2.real + c1.imag * c2.imag) / denominator;
             imag = float(c1.imag * c2.real - c1.real * c2.imag) / denominator;
         }
 
-        void display(){
+        void display(){                    // Display Function
             cout<<real<<" + "<<imag<<"i"<<endl;
         }
     
 };
 
 int main(){
+    //inital
     complex c1, c2, c3;
-    c1.setData(2, 3);
-    c2.setData(4, 4);
+    float r1, r2, i1, i2;
+
+    //getting data from user
+    cout<<"Enter real imaginary part of complex 1 : ";
+    cin>>r1>>i1;
+    cout<<"Enter real imaginary part of complex 2: ";
+    cin>>r2>>i2;
+
+    //Setting C1 and C2
+    c1.setData(r1, i1);
+    c2.setData(r2, i2);
+
+    //Main work
     cout<<"Complex number 1: ";
     c1.display();
     cout<<"Complex number 2: ";
